5e9ec5563663a0f520e6ea35ab6bf4ebed507b85
Sachin wele <115727568+Mrk0p@users.noreply.github.com>
2023-06-15 21:06:36 +0530

    Added : basic problems

---
+++ basic_problems/Calculator.sh
@@ -0,0 +1,14 @@
+read -p "Enter the 1st Number :" a
+read -p "Enter the 2nd Number :" b
+
+echo "Addition of a and b."
+echo $(( $a + $b ))
+
+echo "Subtration of a and b. "
+echo $(( $a - $b ))
+
+echo "Multiplication of a and b. "
+echo $(( $a * $b ))
+
+echo "Division of a and b "
+echo $(( $a / $b ))
---
+++ basic_problems/Name.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+read -p "Enter your First Name :" firstName
+read -p "Enter your Last Naeme :" lastName
+
+echo "Hello $firstName $lastName"
